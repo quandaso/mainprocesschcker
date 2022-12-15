91bebf3aa093ab34ba48c7db7d8ccad7202fc599
quantm <quantm.tb@gmail.com>
2022-12-15 21:45:01 +0700

    Bug fixes

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ let notifyCount = 0;
 
 function timestamp() {
     const d = new Date;
-    return d.getFullYear() + '-' + (d.getMonth() + 1) + '-'+ d.getDate() +
+    return config.APP_NAME + '::' + d.getFullYear() + '-' + (d.getMonth() + 1) + '-'+ d.getDate() +
         ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
 }
 let originalProcesses = [];
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    APP_NAME: process.env.APP_NAME,
     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
     TELEGRAM_ID: process.env.TELEGRAM_ID,
     TZ: process.env.TZ
